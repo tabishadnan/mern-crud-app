b009ae846c4a30b496d9d05533ed200ab3577bff
tabishadnan <tabishadnan000@gmail.com>
2021-02-15 02:36:29 +0500

    mern app host on heroku

--- server.js
+++ server.js
@@ -14,10 +14,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
+
+if(process.env.NODE_ENV=="production"){
+  app.use(express.static('client/build'))
+  const path = require('path')
+  app.get("*",(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
